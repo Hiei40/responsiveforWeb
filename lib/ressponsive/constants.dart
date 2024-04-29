fb242c3b534df1c17ab3808148cd148d5f58ebbd
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-04-29 18:11:35 +0300

    1

---
+++ lib/ressponsive/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+var myAppBar = AppBar(
+  backgroundColor: Colors.grey[900],
+);
+
+var myDrawer = Drawer(
+  backgroundColor: Colors.grey[300],
+  child: const Column(
+    children: [],
+  ),
+);
+//
---
+++ lib/ressponsive/mobile_scaffold.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MobileScaffold extends StatefulWidget {
+  const MobileScaffold({super.key});
+
+  @override
+  State<MobileScaffold> createState() => _MobileScaffoldState();
+}
+
+class _MobileScaffoldState extends State<MobileScaffold> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
