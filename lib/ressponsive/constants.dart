7c993e96f105369205b0316d56a29d52fb8cd986
Mohamed Khaled <k.abdalrhem@gmail.com>
2024-04-30 14:15:27 +0300

    1

--- lib/ressponsive/constants.dart
+++ lib/ressponsive/constants.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 var myAppBar = AppBar(
   backgroundColor: Colors.grey[900],
 );
@@ -7,7 +6,35 @@ var myAppBar = AppBar(
 var myDrawer = Drawer(
   backgroundColor: Colors.grey[300],
   child: const Column(
-    children: [],
+    children: [
+      DrawerHeader(child: Icon(Icons.favorite)),
+      ListTile(
+        leading:  Icon(Icons.home),
+        title: Text("D A S H B O A R D"),
+
+      ),
+      ListTile(
+        leading:  Icon(Icons.chat),
+        title: Text("M E S S A G E"),
+
+      ),
+      ListTile(
+        leading:  Icon(Icons.settings),
+        title: Text("S E T T I N G S"),
+      ),
+      ListTile(
+        leading:  Icon(Icons.logout),
+        title: Text("L O G O U T"),
+
+      ),
+
+
+
+
+
+
+
+    ],
   ),
 );
 //
